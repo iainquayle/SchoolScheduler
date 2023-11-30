9bc97de330d657ba25e8e04eb2ddd5c9d1b3b788
crispyfong <92131641+Crispyfong@users.noreply.github.com>
2023-11-30 16:04:14 -0700

    login func updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,11 @@ import Login from "./pages/login"
 function App() {
   const [userid, setUserid] = createSignal('')
   const [password, setPassword] = createSignal('')
+
+  const handleSubmit = () => {
+    console.log('User ID:', userid())
+    console.log('Password:', password())
+  }
   return (
     <>
       <Login setUserid={setUserid} setPassword={setPassword} />
