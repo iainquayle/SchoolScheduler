f9cdd9df1e13c64df8907b351522ce2c7090d985
iain <iainquayle@outlook.com>
2023-12-03 00:13:19 -0700

    front end notes

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { createSignal } from 'solid-js'
 
 //may want to split the login logic to a seperate module, put it under somehting like api
+//also, could do the login through a modal rather than a page, will need a modal anyway for schedule editing
 export default function Login(setUserid: any, setPassword: any) {
   const [username, setUsername] = createSignal('')
 
