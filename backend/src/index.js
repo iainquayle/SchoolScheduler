00a484ff3f24dee6cc888fc2ca6d9bf95cc0d1d1
crispyfong <efong1299@gmail.com>
2023-12-09 22:54:07 -0700

    fixed css and added backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const userRoutes = require('./userRoutes'); // Adjust the path accordingly
+const db = require('./db'); // Adjust the path accordingly
 
+app.use(express.json());
+
+// Use the userRoutes for requests starting with /users
+app.use('/users', userRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-	console.log('Listening on ${PORT}');
+  console.log(`Listening on ${PORT}`);
 });
---
+++ backend/src/userRoutes.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./db'); // Adjust the path accordingly
+
+// Get all users
+router.get('/', (req, res) => {
+  db.query('SELECT * FROM Users', (err, results) => {
+    if (err) {
+      console.error('Error fetching users:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// Get a specific user by ID
+router.get('/:userId', (req, res) => {
+  const userId = req.params.userId;
+  db.query('SELECT * FROM Users WHERE UserID = ?', [userId], (err, results) => {
+    if (err) {
+      console.error('Error fetching user:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else if (results.length === 0) {
+      res.status(404).json({ error: 'User not found' });
+    } else {
+      res.json(results[0]);
+    }
+  });
+});
+
+// Create a new user (registration)
+router.post('/', (req, res) => {
+  const { Username, Password, FirstName, LastName, email, IsAdmin } = req.body;
+  db.query(
+    'INSERT INTO Users (Username, Password, FirstName, LastName, email, IsAdmin) VALUES (?, ?, ?, ?, ?, ?)',
+    [Username, Password, FirstName, LastName, email, IsAdmin],
+    (err, results) => {
+      if (err) {
+        console.error('Error creating user:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+      } else {
+        res.json({ message: 'User created successfully' });
+      }
+    }
+  );
+});
+
+// Update a user by ID
+router.put('/:userId', (req, res) => {
+  const userId = req.params.userId;
+  const { FirstName, LastName, email, IsAdmin } = req.body;
+  db.query(
+    'UPDATE Users SET FirstName = ?, LastName = ?, email = ?, IsAdmin = ? WHERE UserID = ?',
+    [FirstName, LastName, email, IsAdmin, userId],
+    (err, results) => {
+      if (err) {
+        console.error('Error updating user:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+      } else if (results.affectedRows === 0) {
+        res.status(404).json({ error: 'User not found' });
+      } else {
+        res.json({ message: 'User updated successfully' });
+      }
+    }
+  );
+});
+
+// Delete a user by ID
+router.delete('/:userId', (req, res) => {
+  const userId = req.params.userId;
+  db.query('DELETE FROM Users WHERE UserID = ?', [userId], (err, results) => {
+    if (err) {
+      console.error('Error deleting user:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else if (results.affectedRows === 0) {
+      res.status(404).json({ error: 'User not found' });
+    } else {
+      res.json({ message: 'User deleted successfully' });
+    }
+  });
+});
+
+module.exports = router;
